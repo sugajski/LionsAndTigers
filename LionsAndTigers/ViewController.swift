32ea2b99ee147ac60b69ffe74ab61747a1b142e8
Maciej Sugajski <maciejsugajski@gmail.com>
2016-07-21 09:32:08 +0200

    Initial Commit

---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Maciej Sugajski on 21.07.2016.
+//  Copyright © 2016 Maciej Sugajski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
